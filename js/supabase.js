2c0f524add68127de9ea18cd1d1b129978093c21
wlals0693 <jimin0693@gmail.com>
2026-05-23 14:53:38 +0900

    'db재연결'

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 // js/supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'https://ojplhcukghcmqogcafhj.supabase.co';
+const supabaseUrl = 'https://dtgnuplyrzrwfwuqnuik.supabase.co';
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qcGxoY3VrZ2hjbXFvZ2NhZmhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2OTAzMTYsImV4cCI6MjA5MzI2NjMxNn0.jWh2bcdIwTVj6h3ikrLu7sNnXa2bwo400aTmLUhCq68';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0Z251cGx5cnpyd2Z3dXFudWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1MTAzMTMsImV4cCI6MjA5NTA4NjMxM30.mjNGeTQYoCIXyk1coG3u4n31jp7-bf8lTUI8dhVk1xw';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
